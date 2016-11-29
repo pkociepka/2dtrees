c6ddbd5506bae78a60abec35e91381324d29a026
Piotr Ociepka <pkociepka@users.noreply.github.com>
2016-11-29 18:52:27 +0100

    Create environment.sh

---
+++ environment.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get -y install python-pip
+sudo pip install networkx
+sudo pip install pyforms
+sudo apt-get install -y python-qt4
+sudo apt-get install -y python-matplotlib
+sudo apt-get install -y python-pygraphviz
